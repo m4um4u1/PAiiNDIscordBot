a6ab5df5ffc43eccb56d3754197dee82032f9b61
Maurice <m4um4u1@gmail.com>
2020-11-14 05:16:50 +0100

    made a CommandTemplate so it easier to make new commands

--- CommandTemplate.ts
+++ CommandTemplate.ts
@@ -1,5 +1,5 @@
 import { Command } from 'discord-akairo';
-import { Message } from 'discord.js'
+import { Message } from 'discord.js';
 
 export default class TempCommand extends Command {
     public constructor() {
